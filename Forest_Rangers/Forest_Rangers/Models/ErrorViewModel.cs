d4a113f6f4571afc0c64b60b910ff4bb59f22d4b
RaduTarsa <36694856+RaduTarsa@users.noreply.github.com>
2021-05-22 10:44:37 +0300

    Base Project
    Done.

---
+++ Forest_Rangers/Forest_Rangers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Forest_Rangers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
